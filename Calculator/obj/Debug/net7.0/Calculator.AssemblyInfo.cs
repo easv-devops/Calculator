211e4483bde6ed487f7527822de5c82646f299eb
chrsut01 <chrissu360@gmail.com>
2024-02-03 13:31:09 +0100

    .drone.yml

--- Calculator/obj/Debug/net7.0/Calculator.AssemblyInfo.cs
+++ Calculator/obj/Debug/net7.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f0c09a78aaba01e86c325aa0e149dd99f77fdb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcee54eafb6b828857519f460508395aa7531496")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
